85d9a3752ab8a19d0a3d428d66b775d07f0141e4
sarahsookoo <sarah_sookoo@outlook.com>
2021-11-01 23:50:50 -0400

    algoritm

--- calc.js
+++ calc.js
@@ -1,13 +1,30 @@
 var first_num;
 var second_num;
 
+//find a way to access value of button to have one display function
+//store 2 numbers, get operation, solve
 
-function display(){
+function display0(){
+    //first_num = document.getElementsByName();
     //get number of button
     //display it in box
+    // console.log("Hello world");
+    //alert("Hello world");
+    // first_num += 0;
+    document.getElementById("first").innerHTML = 0;
+
 }
 
-function result(){
-    //on click enter
-    //if *insert operation* then do operation on both numbers
+function display1(){
+    document.getElementById("second").innerHTML = 1;
+
 }
+
+function displayOperation(){
+    
+}
+
+// function result(){
+//     //on click enter
+//     //if *insert operation* then do operation on both numbers
+// }
