65015e60ec633555d1ce73f22361fee601bacc9b
sarahsookoo <64719298+sarahsookoo@users.noreply.github.com>
2021-11-01 22:41:52 -0400

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,5 @@
+Sarah Sookoo
+Start date: 11/1/2021
+
+- Currently working on a simple calculator that can handle the four basic operations.
+-  First step is to have the calculator solve expressions with 2 terms.
---
+++ calc.js
@@ -0,0 +1,13 @@
+var first_num;
+var second_num;
+
+
+function display(){
+    //get number of button
+    //display it in box
+}
+
+function result(){
+    //on click enter
+    //if *insert operation* then do operation on both numbers
+}
